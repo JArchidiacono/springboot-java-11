e16b21b16e3a0067f78a2c51a02f0d8e8d35f0c7
JaimeDev <jaimegaudencio@gmail.com>
2022-03-10 17:42:22 -0300

    Category

---
+++ src/main/java/JaimeDev/course/repositories/CategoryRepository.java
@@ -0,0 +1,8 @@
+package JaimeDev.course.repositories;
+
+import JaimeDev.course.entities.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
