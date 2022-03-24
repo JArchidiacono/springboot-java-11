de4d56f3712dbfc5cf677b2f95dc9b20395af352
JaimeDev <jaimegaudencio@gmail.com>
2022-03-24 13:21:04 -0300

    Exception handling - update

--- src/main/java/JaimeDev/course/resources/UserResource.java
+++ src/main/java/JaimeDev/course/resources/UserResource.java
@@ -4,7 +4,6 @@ import JaimeDev.course.entities.User;
 import JaimeDev.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
