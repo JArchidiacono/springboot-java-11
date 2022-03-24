d823310fefe14f54077e79bb8e32d44a5dbde7a4
JaimeDev <jaimegaudencio@gmail.com>
2022-03-24 13:06:31 -0300

    Exception handling - delete

--- src/main/java/JaimeDev/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/JaimeDev/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package JaimeDev.course.resources.exceptions;
 
+import JaimeDev.course.services.exceptions.DatabaseException;
 import JaimeDev.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +15,16 @@ public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
-
         String error = "Resource not found";
         HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database (DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
         StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
---
+++ src/main/java/JaimeDev/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,10 @@
+package JaimeDev.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+
+}
